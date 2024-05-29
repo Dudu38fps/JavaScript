272b6ec6e5d966b9bab8e2a1522b0484ac5417b1
Eduardo Feliciano <138251065+Dudu38fps@users.noreply.github.com>
2024-05-29 11:37:31 -0300

    aula 12

---
+++ aula12/ex001.js
@@ -0,0 +1,9 @@
+var idade = 19
+
+if (idade < 16) {
+    console.log('Não Vota')
+} else if (idade > 17 && idade < 65) {
+    console.log('Voto Obrigatório')
+} else {
+    console.log('Opcional')
+}
