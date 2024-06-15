9f9b9a193f24f5c6f3c2d8de2a17e05697f2cfcd
Eduardo Feliciano <138251065+Dudu38fps@users.noreply.github.com>
2024-06-15 12:33:24 -0300

    laços de repetição

---
+++ aula14/ambiente.js
@@ -0,0 +1,23 @@
+/* var c = 1 
+
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+var a = 1
+
+do {
+    console.log('Tudo Bem?')
+    a++
+} while(a <= 10)
+    */
+
+var pizza = 1
+
+while (pizza <= 8) {
+    console.log('comer fatia')
+    pizza++
+}
+
+console.log('acabou')
