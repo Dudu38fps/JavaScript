4b3af53005079d0c5ffba0e1f5fb50497adb391d
Eduardo Feliciano <138251065+Dudu38fps@users.noreply.github.com>
2024-07-10 12:55:14 -0300

    funções

---
+++ aula16/funcao06.js
@@ -0,0 +1,17 @@
+function num(n1 = 0, n2 = 0) {
+    return n1 * n2
+} 
+
+console.log(num(9, 9))
+
+
+function parimpar (n) {
+
+    if (n%2 == 0) {
+        return ('Par')
+    } else {
+        return ('Impar')
+    }
+}
+
+console.log (parimpar(66))
