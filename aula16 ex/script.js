bdfbbf2df5d48217949a82370023746949cf4447
Eduardo Feliciano <138251065+Dudu38fps@users.noreply.github.com>
2024-07-13 12:20:24 -0300

    1 parte ex 16

---
+++ aula16 ex/script.js
@@ -0,0 +1,11 @@
+var num = document.getElementById('num').value
+var tela = document.getElementById('tela')
+var res = document.getElementById('res')
+
+let valores = []
+
+function Adicionar() {
+    
+
+    
+}
