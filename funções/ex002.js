90dac2c2871fe29e7a83e984bd9df505b3c4560d
Eduardo Feliciano <138251065+Dudu38fps@users.noreply.github.com>
2024-08-28 13:10:57 -0300

    ex002 parte 1

---
+++ funções/ex002.js
@@ -0,0 +1,4 @@
+/* Currying
+Técnica de transformação de uma função que recebe múltiplos parâmetros de forma que ela pode ser chamada como uma cadeia de funções que recebem um parâmetro cada, ou seja em vez de criar uma função só com todos os parâmetros, pode criar mais funções com parâmetros separados
+
+*/
