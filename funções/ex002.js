c27085176bd83bc50eb66d8541b8a086737fda3e
Eduardo Feliciano <138251065+Dudu38fps@users.noreply.github.com>
2024-09-11 14:32:05 -0300

    funções ex 2

--- funções/ex002.js
+++ funções/ex002.js
@@ -2,3 +2,28 @@
 Técnica de transformação de uma função que recebe múltiplos parâmetros de forma que ela pode ser chamada como uma cadeia de funções que recebem um parâmetro cada, ou seja em vez de criar uma função só com todos os parâmetros, pode criar mais funções com parâmetros separados
 
 */
+
+// Função normal
+
+function pessoa(nome,idade) {
+    let nomeidade = `Meu nome é ${nome}, minha idade é ${idade}`
+
+    return nomeidade
+}
+
+var humano = pessoa('Luana', 3)
+
+console.log(humano)
+
+// Currying
+
+function pessoal(nome) {
+    return function (idade) {
+        return `Meu nome é ${nome} e minha idade é ${idade}`
+    }
+}
+
+var feminino = pessoal('Leticia')
+var masculino = pessoal('Eduardo')
+console.log(feminino(22))
+console.log(masculino(19))
