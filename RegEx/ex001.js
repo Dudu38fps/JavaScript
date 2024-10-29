792bd926a20f219337305a88099f15d8b3f48255
Eduardo Feliciano <138251065+Dudu38fps@users.noreply.github.com>
2024-10-29 14:05:04 -0300

    RegEx ex001

---
+++ RegEx/ex001.js
@@ -0,0 +1,17 @@
+// RegEx
+
+
+let texto = "olá Mundo"
+let reg = /Mundo/g;
+
+let r = texto.match(reg)
+
+console.log(r)
+
+let text = "Este é meu email: ddubr37@gmail.com"
+
+let email = /([a-z0-9]{2,})@([a-z0-9]{2,})(\.[a-z]{2,})(\.[a-z]{2,})?/g
+
+let res = email.exec(text)
+
+console.log(res)
