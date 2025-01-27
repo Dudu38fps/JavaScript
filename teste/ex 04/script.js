a2f57288ac1060b3bccb8277c5692238e8c02388
Eduardo Feliciano <138251065+Dudu38fps@users.noreply.github.com>
2025-01-27 19:07:37 -0300

    ex 04 teste finalizado

--- teste/ex 04/script.js
+++ teste/ex 04/script.js
@@ -1,3 +1,8 @@
+const dadosValidos= {
+    emails : [],
+    numeros : []
+};
+
 const padrao_email = /([a-zA-Z0-9]{2,})@([a-z]{2,})(\.[a-zA-Z0-9]{2,})?/g
 
 const padrao_numero = /^\(\d{2}\) \d{5}-\d{4}$/g
@@ -21,16 +26,20 @@ function Enviar() {
 
     if (validarEmail(email)){
         mensagem += "<p> E-mail válido!</p>"
+        dadosValidos.emails.push(email);
     } else {
         mensagem += "<p>E-mail inválido, certifique que o formato do e-mail está certo.</p>"
     }
 
     if (validarNumero(numero)) {
         mensagem += "<p> Número válido!</p>"
+        dadosValidos.numeros.push(email)
     } else {
         mensagem += "<p> Número inválido, certifique que o número foi digitado no formato correto.</p>"
     }
 
 
    res.innerHTML = mensagem
+
+   console.log("dadosValidos:",JSON.stringify(dadosValidos,null,2));
 }
